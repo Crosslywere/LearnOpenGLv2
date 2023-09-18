d549f640bbed3e16e36a7606ff67cdc140096cea
Crosslywere <ogboruj@outlook.com>
2023-09-18 21:43:56 +0100

    Called OpenGL functions to test

--- LearnOpenGL/Source/main.cpp
+++ LearnOpenGL/Source/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <Window.h>
+#include <glad/glad.h>
+#include <iostream>
 
 int main()
 {
 	if (Window::Create())
+	{
+		glClearColor(0.1f, 0.2f, 0.4f, 1.0f);
 		while (Window::IsRunning())
+		{
+			glClear(GL_COLOR_BUFFER_BIT);
+
 			Window::Update();
+		}
+	}
 	Window::Terminate();
 }
