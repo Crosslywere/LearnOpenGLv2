b96b25f7d8f42c740376d39147edf8da6b1b3904
Crosslywere <ogboruj@outlook.com>
2023-09-18 22:03:13 +0100

    Created a generic buffer interface

---
+++ LearnOpenGLTools/Include/GPUBuffer.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef GPU_BUFFER_H
+#define GPU_BUFFER_H
+
+/**
+ * @brief An abstract buffer of memory allocated on the GPU.
+ * Can be implemented by VertexBuffers, IndexBuffers, etc.
+ */
+class GPUBuffer
+{
+public:
+	GPUBuffer() = default;
+	virtual ~GPUBuffer() = 0;
+	virtual void Bind() const = 0;
+	virtual void Unbind() const = 0;
+protected:
+	unsigned int m_BufferID{ 0 };
+};
+
+#endif // GPU_BUFFER_H
