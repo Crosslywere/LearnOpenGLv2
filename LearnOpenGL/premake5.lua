6d0de94bd4b96332926cfe554d578d52488c832a
Crosslywere <ogboruj@outlook.com>
2023-09-18 21:19:09 +0100

    Specified includes and projects to link

--- LearnOpenGL/premake5.lua
+++ LearnOpenGL/premake5.lua
@@ -7,3 +7,6 @@ targetdir "bin/%{prj.name}/%{cfg.buildcfg}"
 objdir "obj/%{prj.name}/%{cfg.buildcfg}"
 
 files { "**.cpp" }
+includedirs { "../LearnOpenGLTools/Include", "../Vendor/glad/include" }
+
+links { "LearnOpenGLTools" }
