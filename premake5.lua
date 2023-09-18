7eed2524914d09521694ebc2a91d59d2a7f729bc
Crosslywere <ogboruj@outlook.com>
2023-09-18 21:17:39 +0100

    Added all static libraries to main premake

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,9 @@ architecture "x64"
 configurations { "Debug", "Release" }
 
 -- Dependencies (Static Libraries)
+include "Vendor/GLFW"
+include "Vendor/glad"
+include "LearnOpenGLTools"
 
 -- Projects (Executables)
 include "LearnOpenGL"
