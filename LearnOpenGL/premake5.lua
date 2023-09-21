9b6beafecad44b797db497218ede2fa254b1c336
Crosslywere <ogboruj@outlook.com>
2023-09-21 08:10:51 +0100

    Added glm

--- LearnOpenGL/premake5.lua
+++ LearnOpenGL/premake5.lua
@@ -7,6 +7,6 @@ targetdir "bin/%{prj.name}/%{cfg.buildcfg}"
 objdir "obj/%{prj.name}/%{cfg.buildcfg}"
 
 files { "**.cpp" }
-includedirs { "../LearnOpenGLTools/Include", "../Vendor/glad/include", "../Vendor/imgui" }
+includedirs { "../LearnOpenGLTools/Include", "../Vendor/glad/include", "../Vendor/imgui", "../Vendor/glm/include" }
 
 links { "LearnOpenGLTools" }
--- LearnOpenGLTools/premake5.lua
+++ LearnOpenGLTools/premake5.lua
@@ -4,6 +4,6 @@ language "C++"
 cppdialect "C++17"
 
 files { "**.h", "**.cpp" }
-includedirs { "Include", "../Vendor/glad/include", "../Vendor/GLFW/include", "../Vendor/imgui", "../Vendor/imgui/backends" }
+includedirs { "Include", "../Vendor/glad/include", "../Vendor/GLFW/include", "../Vendor/imgui", "../Vendor/imgui/backends", "../Vendor/glm/include" }
 
 links { "glad", "GLFW", "imgui" }
